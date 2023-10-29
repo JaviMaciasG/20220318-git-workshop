2dfe9e75aeb07981114910af6218aa280a4f4906
Javier Macias-Guarasa <javier.maciasguarasa@uah.es>
2023-10-29 22:13:27 +0100

    Added comment & message modified

--- hello-jmg.c
+++ hello-jmg.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-  
+ 
+// This is just a simple comment
 int main(int argc, char **argv)
 {
-    printf("Hello World from Javi Macías!");
+    printf("Hello World from Javi Macías Guarasa!");
   
     return 0;
 }
