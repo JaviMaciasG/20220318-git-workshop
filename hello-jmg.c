491da4edd712382c8ecd404e2de39289febda7dd
Javier Macias-Guarasa <javier.maciasguarasa@uah.es>
2023-10-29 22:44:17 +0100

    Added feature2 function

--- hello-jmg.c
+++ hello-jmg.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
- 
+
+// This is function feature 2
+int feature2(int a, int b)
+{
+	return a+b;
+}
+
 // This is just a simple comment of the code
 int main(int argc, char **argv)
 {
